db64d5bc9369055ef2f0303c7b5cbd895cb8be24
Jacob Hein <jacobhein1892@gmail.com>
2023-05-06 20:52:02 -0700

    updating class to use Map rather than individual elements

--- main.cpp
+++ main.cpp
@@ -1,41 +1,37 @@
 #include <stdio.h>
 #include <iostream>
-#include <string.h>
+#include <fstream>
 #include <string>
+#include <map>
 
-class KeyPair{
+class KeyPair {
     public:
-        KeyPair(std::string type): my_type(type) 
+        KeyPair(std::string filename, std::map<std::string, std::string>& my_map): filename(filename), map(my_map) 
         {
-            if(my_type == "add"){
-                addKeyPair();
-            } 
-            else if (my_type == "edit"){
-                editKeyPair();
+            // When we start a new KeyPair class, we are going to automatically insert the map into a file.
+            std::ofstream file(filename);
+            if (!file.is_open()){
+                std::cerr << "Error could not open file " << filename << std::endl;
+                return;                
             }
-            else if (my_type == "delete"){
-                deleteKeyPair();
+            
+            for (const auto& p : my_map){
+                file << p.first << "," << p.second << std::endl;
             }
-        }
 
+            file.close();
+        }
     private:
-        std::string my_type;
-
-        void addKeyPair(){
-            std::cout << "Adding new Key/Pair" << std::endl;
-        }
-        void editKeyPair(){
-            std::cout << "Edit Key/Pair" << std::endl;
-        }
-        void deleteKeyPair(){
-            std::cout << "Delete Key/Pair" << std::endl;
-        }
+        std::string filename;
+        std::map<std::string, std::string> map;
 };
 
+int main()
+{
+    std::map<std::string, std::string> user_map;
+    user_map["Key1"] = "Val1";
+    user_map["Key2"] = "val2";
+    user_map["Key3"] = "val3";
 
-int main(){
-
-    KeyPair userKeyPair("add");
-
-    return 0;
+    KeyPair newKeyPair("TestFile.txt", user_map);
 }
