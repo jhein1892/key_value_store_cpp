a5ca4c426553ac4fc4d727b530427352e284b8fd
Jacob Hein <jacobhein1892@gmail.com>
2023-05-08 14:56:46 -0700

    able to edit and add pairs in file

--- main.cpp
+++ main.cpp
@@ -16,10 +16,50 @@ class KeyPair {
             }
             
             for (const auto& p : my_map){
-                file << p.first << "," << p.second << std::endl;
+                file << p.first << ":" << p.second << std::endl;
             }
 
             file.close();
+        }
+
+        // Add new Pair value to the txt file
+        void addPair(std::string keyName, std::string value) {
+            std::ofstream outfile(filename, std::ios::app);
+            if(outfile.is_open()){
+                outfile << keyName << "," << value << std::endl;
+                outfile.close();
+            } else {
+                std::cerr << "Error: could not open file for reading" << std::endl;
+            }
+        }
+
+        void editPair(std::string keyName, std::string newValue){
+            std::ifstream inFile(filename);
+            std::string line;
+            int lineNum = 0;
+
+            while(std::getline(inFile, line)){
+                lineNum += 1;
+                std::size_t pos = line.find(":");
+                if(pos != std::string::npos){
+                    std::string key = line.substr(0, pos);
+                    std::string value = line.substr(pos + 1);
+                    if(key == keyName){
+                        std::cout << key << ": " << value << lineNum << std::endl;
+                        map[key] = newValue;
+                    } else {
+                        map[key] = value;
+                    }
+                }
+            }
+            inFile.close();
+
+            std::ofstream outFile(filename);
+            for (const auto& p : map){
+                outFile << p.first << ":" << p.second << std::endl;
+            }
+
+            outFile.close();
         }
     private:
         std::string filename;
@@ -34,4 +74,7 @@ int main()
     user_map["Key3"] = "val3";
 
     KeyPair newKeyPair("TestFile.txt", user_map);
+
+    // newKeyPair.addPair("Key4", "val4");
+    newKeyPair.editPair("Key1", "NewVal1");
 }
