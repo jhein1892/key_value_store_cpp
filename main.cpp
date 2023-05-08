239898e46c8338b5a8c19449d5390dddaa8daff7
Jacob Hein <jacobhein1892@gmail.com>
2023-05-08 15:13:08 -0700

    cleaned up a lot of the logic, separated repetitive code into methods

--- main.cpp
+++ main.cpp
@@ -8,62 +8,63 @@ class KeyPair {
     public:
         KeyPair(std::string filename, std::map<std::string, std::string>& my_map): filename(filename), map(my_map) 
         {
-            // When we start a new KeyPair class, we are going to automatically insert the map into a file.
-            std::ofstream file(filename);
-            if (!file.is_open()){
-                std::cerr << "Error could not open file " << filename << std::endl;
-                return;                
-            }
-            
-            for (const auto& p : my_map){
-                file << p.first << ":" << p.second << std::endl;
-            }
-
-            file.close();
+            updateFile();
         }
 
         // Add new Pair value to the txt file
         void addPair(std::string keyName, std::string value) {
-            std::ofstream outfile(filename, std::ios::app);
-            if(outfile.is_open()){
-                outfile << keyName << "," << value << std::endl;
-                outfile.close();
-            } else {
-                std::cerr << "Error: could not open file for reading" << std::endl;
-            }
+            updateMap();
+            map.insert(std::pair<std::string, std::string>(keyName, value));
+            updateFile();
         }
 
+        // Edit Pair of values in txt file
         void editPair(std::string keyName, std::string newValue){
-            std::ifstream inFile(filename);
-            std::string line;
-            int lineNum = 0;
+            updateMap();
+            map[keyName] = newValue;
+            updateFile();
+        }
 
-            while(std::getline(inFile, line)){
-                lineNum += 1;
-                std::size_t pos = line.find(":");
-                if(pos != std::string::npos){
-                    std::string key = line.substr(0, pos);
-                    std::string value = line.substr(pos + 1);
-                    if(key == keyName){
-                        std::cout << key << ": " << value << lineNum << std::endl;
-                        map[key] = newValue;
-                    } else {
-                        map[key] = value;
-                    }
-                }
-            }
-            inFile.close();
-
-            std::ofstream outFile(filename);
-            for (const auto& p : map){
-                outFile << p.first << ":" << p.second << std::endl;
-            }
-
-            outFile.close();
+        void deletePair(std::string keyName){
+            std::cout << "Delete" << std::endl;
         }
     private:
         std::string filename;
         std::map<std::string, std::string> map;
+
+        // Reads File, and updates local map to reflect contents
+        void updateMap(){
+            std::ifstream inFile(filename);
+            std::string line;
+
+            if(!inFile.is_open()){
+                std::cerr << "Error: Could not open file " << filename << std::endl;
+                return;
+            }
+
+            while(std::getline(inFile, line)){
+                std::size_t pos = line.find(":");
+                if(pos != std::string::npos){
+                    std::string key = line.substr(0, pos);
+                    std::string value = line.substr(pos + 1);
+                    map[key] = value;
+                }
+            }
+            inFile.close();
+        };
+
+        void updateFile(){
+            std::ofstream outFile(filename);
+            if(!outFile.is_open()){
+                std::cerr << "Error: Could not open File " << filename << std::endl;
+                return;
+            }
+            for (const auto& p : map){
+                outFile << p.first << ":" << p.second << std::endl;
+            }
+            outFile.close();
+        };
+        
 };
 
 int main()
@@ -75,6 +76,6 @@ int main()
 
     KeyPair newKeyPair("TestFile.txt", user_map);
 
-    // newKeyPair.addPair("Key4", "val4");
+    newKeyPair.addPair("Key4", "val4");
     newKeyPair.editPair("Key1", "NewVal1");
 }
